b68ff6931307c55666431eec8646f35d63a108e8
realFK98 <wishira2013.coth@gmail.com>
2022-11-20 20:10:58 +0700

    ไม่ให้เลื่อนแนวเอ็กซ์

--- greek_dargon/src/HomePage.js
+++ greek_dargon/src/HomePage.js
@@ -10,40 +10,17 @@ import Homepage2 from './HomePage2';
 
 function Homepage() {
     return (
-        <div >
-            <Parallax pages={2} style={{ top: '0', left: '0' }}>
-                <ParallaxLayer
-                    offset={0}
-                    speed={2.5}
-                >
+        <div class='containner'>
+            <img class='could' src={could} alt='dargon1' ></img>
+            <div class='stars'></div>
 
-                    <div class='containner'>
-                        <img class='could' src={could} alt='dargon1' ></img>
-                        <div class='stars'></div>
+            <img class='moon' src={moon} alt='moon' ></img>
+            <img class='dragonImg' src={dargon} alt='dargon1' ></img>
+            <p class='text1HomePage'>LERNAEAN HYDRA</p>
 
-                        <img class='moon' src={moon} alt='moon' ></img>
-                        <img class='dragonImg' src={dargon} alt='dargon1' ></img>
-                        <p class='text1HomePage'>LERNAEAN HYDRA</p>
-
-                        <div class='buttonStart'>
-                            <p class='buttontextStart'>START!</p>
-                        </div>
-                    </div>
-
-
-
-
-                </ParallaxLayer>
-                <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
-
-                <ParallaxLayer
-                    offset={1}
-                    speed={0.5}
-                    style={{}}>
-                    <Homepage2 />
-                </ParallaxLayer>
-            </Parallax>
-
+            <div class='buttonStart'>
+                <p class='buttontextStart'>START!</p>
+            </div>
         </div>
     );
 
