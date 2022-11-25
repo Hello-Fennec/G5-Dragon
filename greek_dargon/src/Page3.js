8b4303cf104c187d54f980ab9db4d7f2e5251f0f
kayjirat <117822880+kayjirat@users.noreply.github.com>
2022-11-26 01:40:20 +0700

    GalleryPage3

--- greek_dargon/src/Page3.js
+++ greek_dargon/src/Page3.js
@@ -1,7 +1,10 @@
 import './Page3.css'
 import './Page2Swipe'
+import './DrawerAppBar'
 import * as React from 'react';
 import './App.css';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import bg from './image2/bg.png';
 import bookleft from './image2/bookleft.png';
 import bookright from './image2/bookright.png';
@@ -20,11 +23,19 @@ import stone from './image2/stone.png';
 import wave from './image2/wave.png';
 import wave1 from './image2/wave1.png';
 import white from './image2/white.png';
+import DrawerAppBar from './DrawerAppBar';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 
 function Page3() {
     return(
     <div className="Page3">
+        <div> <DrawerAppBar/> </div>
         <div class="bg">
             <img className="bg" src={bg} alt="bg"/>
         </div>
@@ -146,14 +157,43 @@ function Page3() {
             </div>
             {/* <!-- ทำให้น้ำขยับได้ --> */}
 
+            <div className="framegallery"></div>
             <div className="GALLERY">
                 <p>GALLERY OF LERNAEAN HYDRA</p>
             </div>
-            {/* <!-- รูปเลื่อนได้5รูป รูปregtangle1-5 --> */}
+            <div className="Swipe3">
+            <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={20}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+        >
+            <SwiperSlide ><span>
+            <img className="Rectangle10" src={Rectangle10} alt="Rectangle10"/>
+            </span></SwiperSlide>
+
+            <SwiperSlide><span>
+            <img className="Rectangle11" src={Rectangle11} alt="Rectangle11"/>
+            </span></SwiperSlide>
+
+            <SwiperSlide><span>
+            <img className="Rectangle12" src={Rectangle12} alt="Rectangle12"/>
+            </span></SwiperSlide>
+
+            <SwiperSlide><span>
+            <img className="Rectangle13" src={Rectangle13} alt="Rectangle13"/>
+            </span></SwiperSlide>
+            ...
+        </Swiper>
+        </div>
 
             <div className="ABOUTUS">
-                <p>ABOUT US </p>
-                {/* <!-- เชื่อมปุ่มไปหน้า4 --> */}
+                <p>ABOUT US >> </p>
             </div>
         </div>
 </div>
