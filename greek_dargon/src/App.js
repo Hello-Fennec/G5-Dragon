6177c5ffdd79d5a37343062dde76a45acdee8c7a
realFK98 <102861732+realFK98@users.noreply.github.com>
2022-11-25 23:29:44 +0700

    Revert "Page2"
    This reverts commit c71fa53210211dc0c9cba84575ab84fccb519814.

--- greek_dargon/src/App.js
+++ greek_dargon/src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import DrawerAppBar from './Hearder';
 import Homepage from './HomePage';
-import Page2 from './Page2';
+
 
 function App() {
   return (
     <div>
-      {/* <DrawerAppBar /> */}
-      {/* <Homepage /> */}
-      <Page2/> 
+      <DrawerAppBar />
+      <Homepage />
     </div>
   );
 }
