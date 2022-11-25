a7068573e5a648d0873cfe4c3bf76806885631e0
realFK98 <102861732+realFK98@users.noreply.github.com>
2022-11-25 23:41:56 +0700

    det

--- greek_dargon/src/App.js
+++ greek_dargon/src/App.js
@@ -1,13 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import DrawerAppBar from './Header';
-import Homepage from './HomePage';
-import Homepage2 from './HomePage2';
 import MN from './MN';
 import NT from './NT';
-import HomrPageReal from './HomrPageReal';
-import Mee from './Mee';
-import Page3 from './Page3';
+import HomrPageReal from './HomrPage';
 
 
 
@@ -17,8 +13,7 @@ function App() {
     <div>
 
       <DrawerAppBar />
-      {/* < HomrPageReal /> */}
-      <Page3/>
+      < HomrPageReal />
 
 
     </div>
--- greek_dargon/src/Header.js
+++ greek_dargon/src/Header.js
@@ -6,14 +6,13 @@ import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { fontFamily } from '@mui/system';
+import "./App.css";
+import "./navbar.css";
+
 //edited
 //Add fontAwesome icon
 
